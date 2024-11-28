1d76badad92b5bd83fd684e4c1824b5b9b0a1619
MatatteoPetrault <matteo.petrault12@gmail.com>
2024-11-28 14:46:15 +0100

    index change

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,26 +1,7 @@
+@extends('layouts.default')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <title>Posts</title>
-</head>
-<body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-    <div class="container-fluid">
-      <a class="navbar-brand h1" href={{ route('posts.index') }}>CRUD</a>
-      <div class="justify-end ">
-        <div class="col ">
-          <a class="btn btn-sm btn-success" href={{ route('posts.create') }}>Ajouter un Role</a>
-        </div>
-      </div>
-    </div>
-  </nav>
-  <div class="container mt-5">
+@section('contenu')
+<div class="container mt-5">
     <div class="row">
       @foreach ($posts as $post)
         <div class="col-sm">
@@ -51,5 +32,8 @@ integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6
       @endforeach
     </div>
   </div>
-</body>
-</html>
+@stop 
+@section('pied')
+
+<footer>pied de page</footer>
+@stop
